f8d28db0b77f4cb0025cae161d52280b40ec2a21
fw2568 <f.wagner@wagnersolutions.de>
2019-07-11 18:52:36 +0200

    gem setup (#1)

---
+++ lib/haipa/client.rb
@@ -0,0 +1,8 @@
+require "haipa/client/version"
+
+module Haipa
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/haipa/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Haipa::Client do
+  it "has a version number" do
+    expect(Haipa::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
